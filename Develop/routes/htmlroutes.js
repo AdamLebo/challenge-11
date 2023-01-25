b4aab04cb5aae229936ab75252f211299ff617e1
AdamLebo <alieberman10@aol.com>
2023-01-25 14:57:37 -0500

    added htmlroutes

---
+++ Develop/routes/htmlroutes.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
